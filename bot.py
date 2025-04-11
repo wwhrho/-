4ee3ab7f020ac4005baf6dcd138c790553bb678b
wwhrho <wwhrho@gmail.com>
2025-04-11 21:16:43 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,10 +15,18 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 GUILD_ID = 1278677581209796618
 AUTH_CHANNEL_ID = 1358474101672382696
 ROLE_NAME = "자유시민"
+UNVERIFIED_ROLE_NAME = "Undocumented"
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} 로그인 완료")
+
+@bot.event
+async def on_member_join(member):
+    guild = bot.get_guild(GUILD_ID)
+    role = discord.utils.get(guild.roles, name=UNVERIFIED_ROLE_NAME)
+    if role:
+        await member.add_roles(role)
 
 @bot.event
 async def on_message(message):
@@ -37,8 +45,11 @@ async def on_message(message):
         member = guild.get_member(message.author.id)
         role = discord.utils.get(guild.roles, name=ROLE_NAME)
 
-        if role:
+        if role and member:
             await member.add_roles(role)
+            undocumented_role = discord.utils.get(guild.roles, name=UNVERIFIED_ROLE_NAME)
+            if undocumented_role in member.roles:
+                await member.remove_roles(undocumented_role)
             await member.edit(nick=nickname)
 
         await message.delete()
